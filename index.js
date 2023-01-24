6ec37c96f0b036dc6846a44775fa4656e6fe84f3
Gianna Mastro <giannamastro02@gmail.com>
2023-01-24 13:12:24 -0500

    commit tree 69241a03e10c72c4d37944369d58d853e87cd4dd

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "Stan Marsh",
+    streetAddress: "123 Main Street, South Park, CO 80440"
+
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+}
+updateEmployeeWithKeyAndValue(employee, "Zipcode", "80443");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "12 Broadway");
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj2 = { ...employee[key] }
+    delete newObj2[key]
+    return newObj2
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
